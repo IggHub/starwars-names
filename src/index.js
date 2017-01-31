007b68d6e0bf79a8f5e3ecbd8cd7fef0da9f8deb
Iggy <igoririanto@rocketmail.com>
2017-01-30 21:27:51 -0800

    adding library

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import uniqueRandomArray from 'unique-random-array';
-import starWarsNames from './starwars-names.json';
-
-const getRandomItem = uniqueRandomArray(starWarsNames);
+var uniqueRandomArray = require('unique-random-array')
+var starWarsNames = require('./starwars-names.json')
 
 module.exports = {
   all: starWarsNames,
-  random: random
+  random: uniqueRandomArray(starWarsNames)
 };
-
-function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    const randomItems = [];
-    for (let i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
-    }
-    return randomItems;
-  }
-}
